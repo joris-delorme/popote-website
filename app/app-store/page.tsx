5a9580b9dc28de5a323a293d8bb163ea08a5bc58
joris-delorme <joris.delorme@epitech.digital>
2024-04-24 10:22:55 +0200

    app-store link

--- app/app-store/page.tsx
+++ app/app-store/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next"
 import { redirect } from "next/navigation"
+
+export const metadata: Metadata = {
+    title: "Popote dans l'App Store"
+}
 
 export default function page () { redirect("https://apps.apple.com/fr/app/popote/id6478100865") }
