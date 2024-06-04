563fd97ab117974b5c31177dcb96c479790b3087
joris-delorme <joris.delorme@epitech.digital>
2024-06-04 07:52:20 +0200

    some seoé

--- next.config.mjs
+++ next.config.mjs
@@ -8,11 +8,7 @@ const nextConfig = {
                 port: '',
                 pathname: '/storage/v1/object/**',
             },
-<<<<<<< HEAD
             {
-=======
-{
->>>>>>> 124b5be698e8ec9653c853601d763fe4fc8e5fe7
                 protocol: 'https',
                 hostname: 'oaidalleapiprodscus.blob.core.windows.net',
                 port: '',
