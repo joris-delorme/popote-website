d943a6a1230731d38e7c59abdff7d13e0c540c7b
Code5urfer <herve.juge@me.com>
2024-07-02 18:52:42 +0200

    24184-18-Modif site config
    Ajout www.

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export const siteConfig = {
   name: "Popote",
-  description: "Popote est une application IOS qui utilise la science et l'IA pour améliore votre bien-être quotidien en créant des recettes de cuisine qui s'adaptent à vos besoins.",
-  url: "https://popote.app",
-  ogImage: "https://popote.app/og.jpg",
+  description: "Popote est une application iOS qui utilise la science et l'IA pour améliorer votre bien-être quotidien en créant des recettes de cuisine qui s'adaptent à vos besoins.",
+  url: "https://www.popote.app",
+  ogImage: "https://www.popote.app/og.jpg",
   links: {
   },
 }
